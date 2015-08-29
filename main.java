dc939a1c41f6c2c84c9465600e5fbf25111ce349
Su Gi Chandran <sollercoaster@hotmail.com>
2015-08-29 14:46:04 +0800

    Added main class

---
+++ main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class main {
+		// User selects architecture type here
+	   public static void main(String args[]) {
+		   Scanner in = new Scanner(System.in);
+		   
+	   }
+}
