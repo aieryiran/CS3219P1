26496b21b5a7ce87c64881b9ff3336c1270c3865
Su Gi Chandran <sollercoaster@hotmail.com>
2015-09-03 12:06:15 +0800

    Capitalize first word of output

--- src/SharedData/output.java
+++ src/SharedData/output.java
@@ -2,9 +2,16 @@ package SharedData;
 
 public class output {
 	public void printTitles(String[] titles){
+		String title;
+		int first_word;
 		System.out.println("Outputting list of circularly shifted titles:");
+		
 		for(int i = 0; i < titles.length; i++){
-			System.out.println(titles[i]);
+			// Capitalize the first word
+			first_word = titles[i].indexOf(" ");
+			title = titles[i].substring(0, first_word).toUpperCase() + titles[i].substring(first_word);
+			
+			System.out.println(title);
 		}
 	}
 }
